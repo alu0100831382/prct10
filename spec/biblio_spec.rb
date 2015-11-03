54938564c48ef9dccc5c000b5f8dcd514e366ca3
alu0100831382 <alu0100831382@ull.edu.es>
2015-11-03 14:58:55 +0000

    Fallo en el titulo de la bibliografia

--- spec/biblio_spec.rb
+++ spec/biblio_spec.rb
@@ -8,13 +8,15 @@ describe Bibliography do
     end
         
     describe "Book exists" do
-        
         it "Author is correct" do
-            
             @book.author.should eq("Author")
             
-            
         end
-        
+    end
+    
+    describe "Book exists" do
+        it "Tittle is correct" do
+            @book.tittle.should eq("Tittle")
+        end
     end
 end
