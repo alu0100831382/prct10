1d366d4566cdb63b03c91ec6cfaf5fcf48feab4d
alu0100831382 <alu0100831382@ull.edu.es>
2015-11-03 12:31:43 +0000

    Fallo en el autor de la bibliografia

--- lib/biblio/bibliography.rb
+++ lib/biblio/bibliography.rb
@@ -1,6 +1,5 @@
 class Bibliography
     
-    
-    
-    
+  
+        
 end
--- spec/biblio_spec.rb
+++ spec/biblio_spec.rb
@@ -1,1 +1,13 @@
+require "biblio/bibliography.rb"
 
+describe Bibliography do
+    
+    before :each do
+        @book = Bibliography.new("Dave Thomas")
+    end
+        
+    describe "Book exists" do
+        it "Author is correct"
+        @book.author.should eq("Dave Thomas")
+    end
+end
