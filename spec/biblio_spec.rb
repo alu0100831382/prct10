41f044a23dbe13a644706c8e213eced214058b6f
alu0100812503 <alu0100812503@ull.edu.es>
2015-11-03 16:45:02 +0000

    Arreglando errores

--- spec/biblio_spec.rb
+++ spec/biblio_spec.rb
@@ -8,44 +8,44 @@ describe Bibliography do
     end
         
     describe "Book exists" do
-        it "Author is correct" do
+        it "Author is incorrect" do
             @book.author.should eq("Dave Thomas")
             
         end
     end
     
     describe "Book exists" do
-        it "Title is correct" do
+        it "Title is incorrect" do
             @book.title.should eq("Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide")
         end
     end
     
     describe "Book exists" do
-        it "Date is correct" do
+        it "Date is incorrect" do
             @book.date.should eq("July 7 2013")
         end
     end
     
     describe "Book exists" do
-        it "Editorial is correct" do
+        it "Editorial is incorrect" do
             @book.editorial.should eq("Pragmatic Bookshelf")
         end
     end
     
     describe "Book exists" do
-        it "Isbn is correct" do
+        it "Isbn is incorrect" do
             @book.isbn10.should eq("1937785491")
         end
     end
     
     describe "Book exists" do
-        it "Edition number is correct" do
+        it "Edition number is incorrect" do
             @book.edition.should eq("4")
         end
     end
     
     describe "Book exists" do
-        it "Series number is correct" do
+        it "Series number is incorrect" do
             @book.series.should eq(" ")
         end
     end
