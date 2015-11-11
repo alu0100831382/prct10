5697c204f77a15a520c7e39672e9923fed184636
alu0100831382 <alu0100831382@ull.edu.es>
2015-11-11 21:33:08 +0000

    Prueba correcta con el primer nodo

--- lib/biblio/lista.rb
+++ lib/biblio/lista.rb
@@ -2,4 +2,11 @@ Node = Struct.new(:value, :next)
 
 class Lista
     
+    attr_accessor :inicio
+    
+    def initialize(node)
+        @inicio = node
+    end
+    
+    
 end
--- spec/biblio_spec.rb
+++ spec/biblio_spec.rb
@@ -9,11 +9,11 @@ describe Biblio do
   
 describe Bibliography do
     
-   l1 = Biblio.new(["Dave Thomas","Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0","(The Facets of Ruby)","Pragmatic Bookshelf", "4 edition", "(July 7, 2013)",["ISBN-13: 978-1937785499", "ISBN-10:1937785491"])
-   l2 = Biblio.new("Scott Chacon","Pro Git 2009th Edition","Apress","2009 edition","(August 27, 2009)",["ISBN-13: 978-1430218333. ISBN-10: 1430218339"])
-   l3 = Biblio.new(["David Flanagan, Yukihiro Matsumoto"],"The Ruby Programming Language","O’Reilly Media","1 edition","(February4, 2008)",["ISBN-10: 0596516177. ISBN-13: 978-0596516178"])
-   l4 = Biblio.new(["David Chelimsky, Dave Astels, Bryan Helmkamp, Dan North, Zach Dennis, Aslak Hellesoy"],"The RSpecBook: Behaviour Driven Development with RSpec","Pragmatic Bookshelf","1 edition","(December 25, 2010)",["ISBN-10: 1934356379. ISBN-13: 978-1934356371"])
-   l5 = Biblio.new("Richard E. Silverman","Git Pocket Guide","O’Reilly Media","1 edition","(August 2, 2013)(August 2, 2013)",["ISBN-10: 1449325866. ISBN-13: 978-1449325862"])
+   l1 = Bibliography.new(["Dave Thomas","Andy Hunt", "Chad Fowler"],"Programming Ruby 1.9 & 2.0. (The Facets of Ruby)","Pragmatic Bookshelf","4 edition","(July 7, 2013)",["ISBN-13: 978-1937785499", "ISBN-10:1937785491"],"")
+   l2 = Bibliography.new("Scott Chacon","Pro Git 2009th Edition","Apress","2009 edition","(August 27, 2009)",["ISBN-13: 978-1430218333. ISBN-10: 1430218339"],"")
+   l3 = Bibliography.new(["David Flanagan, Yukihiro Matsumoto"],"The Ruby Programming Language","O’Reilly Media","1 edition","(February4, 2008)",["ISBN-10: 0596516177. ISBN-13: 978-0596516178"],"")
+   l4 = Bibliography.new(["David Chelimsky, Dave Astels, Bryan Helmkamp, Dan North, Zach Dennis, Aslak Hellesoy"],"The RSpecBook: Behaviour Driven Development with RSpec","Pragmatic Bookshelf","1 edition","(December 25, 2010)",["ISBN-10: 1934356379. ISBN-13: 978-1934356371"],"")
+   l5 = Bibliography.new("Richard E. Silverman","Git Pocket Guide","O’Reilly Media","1 edition","(August 2, 2013)",["ISBN-10: 1449325866. ISBN-13: 978-1449325862"],"")
    
     describe Node do
        
