a2ec6d1d6fda6d0b7a66c165fcf946aff4711912
alu0100831382 <alu0100831382@ull.edu.es>
2015-11-19 15:50:58 +0000

    Creando estructura y arreglando codigo

--- lib/biblio/fichero.rb
+++ lib/biblio/fichero.rb
@@ -1,37 +1,11 @@
 class Bibliography
     
-    attr_accessor :author,:title, :date, :editorial, :isbn10, :edition, :series
+    attr_accessor :author,:title, :date
     
-    def initialize (autor,title,date,editorial,isbn10,edition,series)
+    def initialize (autor,title,date)
         @author = autor 
         @title = title
         @date = date
-        @editorial = editorial
-        @isbn10 = isbn10
-        @edition = edition 
-        @series = series
-    end
-    
-    def to_s 
-        
-        if @author.is_a? Array and @isbn10.is_a? Array
-        "#{@author.join(", ")}\n #{@title}\n #{@editorial}; #{@edition} #{@date}\n #{@isbn10.join(", ")}\n"
-        
-        
-        elsif @isbn10.is_a? Array
-        
-        "#{@author}\n #{@title}\n #{@editorial}; #{@edition} #{@date}\n #{@isbn10.join(", ")}\n"
-        
-        
-        elsif @author.is_a? Array
-        
-        "#{@author.join(", ")}\n #{@title}\n #{@editorial}; #{@edition} #{@date}\n #{@isbn10}\n"
-        
-        else
-            "#{@author}\n #{@title}\n #{@editorial}; #{@edition} #{@date}\n #{@isbn10}\n"
-            
-        end
-         
     end
 
 end
--- spec/biblio_spec.rb
+++ spec/biblio_spec.rb
@@ -65,44 +65,5 @@ describe List do
     end
     
 end
-    
-
-=begin before :each do
-        @book = Bibliography.new(["Dave Thomas","Andy Hunt","Chad Fowler"],"Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide","July 7 2013","Pragmatic Bookshelf",["1937785491","978-1937785499"],"4"," ")
-    end
-    
-    it "Author is correct" do
-        expect(@book.author).to eq(["Dave Thomas","Andy Hunt","Chad Fowler"])
-    end
-   
-    it "Title is correct" do
-        expect(@book.title).to eq("Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide")
-    end
-    
-    it "Date is correct" do
-        expect(@book.date).to eq("July 7 2013")
-    end
-    
-    it "Editorial is correct" do
-        expect(@book.editorial).to eq("Pragmatic Bookshelf")
-    end
-    
-    it "Isbn is correct" do
-        expect(@book.isbn10).to eq(["1937785491","978-1937785499"])
-    end
-  
-    it "Edition number is correct" do
-        expect(@book.edition).to eq("4")
-    end
-    
-    it "Series number is correct" do
-        expect(@book.series).to eq(" ")
-    end
-    
-    it "correct" do
-        expect(@book.to_s).to eq("Dave Thomas, Andy Hunt, Chad Fowler\n Programming Ruby 1.9 & 2.0: The Pragmatic Programmers’ Guide\n Pragmatic Bookshelf; 4 July 7 2013\n 1937785491, 978-1937785499\n" )
-
-end
-=end
 end
 end
